e0aa43056b6be1869a2e3cfb560cfb56952b67e2
Keara Harris <keara.s.harris@gmail.com>
2023-04-22 11:02:10 -0500

    Repeat JS looping code along complete

--- index.js
+++ index.js
@@ -3,28 +3,28 @@
 
 const gifts = ["teddy bear", "drone", "doll"];
 function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-  }
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
     return gifts;
 }
-wrapGifts(gifts); 
+
 
 //Write a function that returns an array of thank you messages at each iteration
 
 function writeCards(names, event) {
     let messageArray = [];
-    for(let i = 0; i < names.length; i++){
-        messageArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);  
-    
+    for (let i = 0; i < names.length; i++) {
+        messageArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+
     }
     return messageArray;
 }
-console.log(writeCards(["Alex", "Jane", "John"], "birthday"))
+
 
 //Write a function that takes in a positive integre, starting from that number, count down to zero using console.log()
 
-const countDown = (n) =>{ 
+const countDown = (n) => {
     let decreaseToZero = n;
     while (decreaseToZero >= 0) {
         console.log(decreaseToZero--)
